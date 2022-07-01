89e073d4e6ad70cfe9e3e3dab5f542389ca7e591
Thomas Thornton <t.thornton@kainos.com>
2022-07-01 12:29:26 +0100

    Update main.tf

--- Terraform-azapi-update-resource/terraform/main.tf
+++ Terraform-azapi-update-resource/terraform/main.tf
@@ -20,12 +20,6 @@ resource "azurerm_media_services_account" "example" {
     id         = azurerm_storage_account.example.id
     is_primary = true
   }
-
-  #     identity {
-  #     type         = "UserAssigned"
-  #     identity_ids = [azurerm_user_assigned_identity.example.id]
-  #   }
-
 }
 
 resource "azapi_update_resource" "test" {
@@ -49,20 +43,6 @@ resource "azapi_update_resource" "test" {
     }
   })
 }
-
-# resource "azapi_update_resource" "test" {
-#   type        = "Microsoft.Media/mediaservices@2021-06-01"
-#   resource_id = azurerm_media_services_account.example.id
-
-#   body = jsonencode({
-#     identity = {
-#       "type" = "UserAssigned",
-#       "userAssignedIdentities" = "/subscriptions/04109105-f3ca-44ac-a3a7-66b4936112c3/resourceGroups/tam-media-resources/providers/Microsoft.ManagedIdentity/userAssignedIdentities/tam-identity"
-#     }
-#   })
-# }
-
-
 
 resource "azurerm_user_assigned_identity" "example" {
   location            = azurerm_resource_group.example.location
