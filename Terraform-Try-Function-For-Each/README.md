7d7e6c4e95d927f45242093f2425f12ef13b5e35
Thomas Thornton <t.thornton@kainos.com>
2024-07-11 15:25:36 +0100

    add blog post try function with for_each in terraform (#90)
    Co-authored-by: Thomas Thornton <>

---
+++ Terraform-Try-Function-For-Each/README.md
@@ -0,0 +1,5 @@
+## Using try function with for_each in Terraform
+
+The try function combined with for_each in Terraform offers a great approach to handling multiple variations in data structures within Terraform. In this blog post, we will look at using both these features to develop more resilient and adaptable Terraform configurations and will also include an example of this usage
+
+[Blog post here](https://thomasthornton.cloud/2021/07/07/terraform-module-does-not-declare-a-provider-warning/)
---
+++ Terraform-Try-Function-For-Each/main.tf
@@ -0,0 +1,57 @@
+locals {
+  nsgrules = {
+    rdp = {
+      name                       = "rdp"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "3389"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    AD = {
+      name                         = "AD"
+      priority                     = 110
+      direction                    = "Inbound"
+      access                       = "Allow"
+      protocol                     = "Tcp"
+      source_port_range            = "*"
+      destination_port_range       = "53,389,636"
+      source_address_prefixes      = ["10.100.52.39/32", "10.100.52.49/32"]
+      destination_address_prefixes = ["10.100.52.10/32", "10.100.62.10/32"]
+    }
+  }
+}
+
+resource "azurerm_resource_group" "tamopsrg" {
+  name     = "tamopsrg"
+  location = "uksouth"
+}
+
+resource "azurerm_network_security_group" "tamopsnsg" {
+  name                = "tamopsnsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_security_rule" "testrules" {
+  for_each = local.nsgrules
+
+  name                   = each.value.name
+  priority               = each.value.priority
+  direction              = each.value.direction
+  access                 = each.value.access
+  protocol               = each.value.protocol
+  source_port_range      = each.value.source_port_range
+  destination_port_range = each.value.destination_port_range
+
+  source_address_prefixes      = try(each.value.source_address_prefixes, null)
+  source_address_prefix        = try(each.value.source_address_prefix, null)
+  destination_address_prefixes = try(each.value.destination_address_prefixes, null)
+  destination_address_prefix   = try(each.value.destination_address_prefix, null)
+
+  resource_group_name         = azurerm_resource_group.tamopsrg.name
+  network_security_group_name = azurerm_network_security_group.tamopsnsg.name
+}
